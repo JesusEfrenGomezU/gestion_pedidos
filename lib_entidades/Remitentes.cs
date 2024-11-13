f65da0bf671e071d4e7d4970191f90796455620f
Miguel <jumiguto@gmail.com>
2024-11-13 07:33:19 -0500

    v.Ac0

--- lib_entidades/Remitentes.cs
+++ lib_entidades/Remitentes.cs
@@ -12,5 +12,13 @@ namespace lib_entidades
         [Key]public int Id { get => this.id; set => this.id = value; }
         public string Nombre { get => this.nombre; set => this.nombre = value; }
         public string Direcc_rem { get => this.direcc_rem; set => this.direcc_rem = value; }
+
+        public bool Validar()
+        {   if (string.IsNullOrEmpty(nombre) ||
+                this.id <= 0 ||
+                string.IsNullOrEmpty(direcc_rem))
+                return false;
+            return true;
+        }
     }
 }
