64cc2071f0b2d246673ebbc39ffd639d45c778e8
Miguel_G <juangutierrez315760@correo.itm.edu.co>
2024-11-21 09:57:18 -0500

    v.Ae

---
+++ lib_entidades/Modelos/Usuarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_entidades.Modelos
+{
+    public class Usuarios
+    {
+        private int id = 0;
+        private string usuario = "";
+        private string password = "";
+
+        [Key] public int Id { get => this.id; set => this.id = value; }
+        private string? Usuario { get => this.usuario; set => this.usuario = value; }
+        private string? Password { get => this.password; set => this.password = value; }
+
+        public bool Validar()
+        {
+            if (this.id <= 0 ||
+                string.IsNullOrEmpty(Usuario) ||
+                string.IsNullOrEmpty(Password)) 
+                return false;
+            return true;
+        }
+    }
+}
